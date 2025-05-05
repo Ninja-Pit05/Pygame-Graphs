a08bc1e8d1257376fb39935229e988e6ab86d068
Ninja-Pit05 <ro314nald@gmail.com>
2025-05-05 10:02:25 -0300

    Update setup.py
    Fix for pkg.pkg

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setuptools.setup(
      name="PythonGraph",     
      version="0.0.1",
      python_requires=">=3.6",   
-     py_modules=["pygameGraph/pygameGraph"]
+     packages=["pygameGraph"],
 )
